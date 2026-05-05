9d6fdfdf0dc8385e965e3593c7657868a978bd11
Aditya Khamkar <adityakhamkar76@gmail.com>
2026-05-05 07:38:52 +0530

    genre column added

--- data cleaning method/dataCleaning.py
+++ data cleaning method/dataCleaning.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
 # Read the CSV file into a DataFrame
-df = pd.read_csv("C:\All my projects\recommendation system real\data cleaning method.csv")
+df = pd.read_csv("IMDbRatings_IndianMovies.csv")
 
 # Remove rows with any missing (NaN) values
 df_clean = df.dropna()
-
 # Replace empty strings with NaN, then drop those rows as well
 df_clean = df_clean.replace('', pd.NA).dropna()
 
 # Create a list of all columns except 'Name'
 cols_except_name = [col for col in df_clean.columns if col != 'Name']
-
 # Remove duplicate rows based on all columns except 'Name'
 df_clean = df_clean.drop_duplicates(subset=cols_except_name)
 
